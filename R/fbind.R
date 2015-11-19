40cd1df268024c0542c3c0744289f1f033556fb2
namanpaul <naman.paul@gmail.com>
2015-11-18 18:56:26 -0800

    add roxygen format
    - remove old binding example

--- R/fbind.R
+++ R/fbind.R
@@ -1,13 +1,15 @@
-(a <- factor(c("character", "hits", "your", "eyeballs")))
-#> [1] character hits      your      eyeballs
-#> Levels: character eyeballs hits your
-(b <- factor(c("but", "integer", "where it", "counts")))
-#> [1] but      integer  where it counts
-#> Levels: but counts integer where it
-c(a, b)
-#> [1] 1 3 4 2 1 3 4 2
-
 #fbind function
+#' Bind two factors
+#' Create a new factor from two existing factors, where the new factor's levels
+#' are the union of the levels of the input factors.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
