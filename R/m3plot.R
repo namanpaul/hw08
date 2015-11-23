7fa3e76f10581ba7098469558cf94ebe9bba3533
namanpaul <naman.paul@gmail.com>
2015-11-22 16:06:22 -0800

    some changes

--- R/m3plot.R
+++ R/m3plot.R
@@ -1,24 +1,24 @@
 #Assuming we have numeric factors, in this case, which would be a bunch of numbers
-var1 <- c(1,23,4,55,6,7,89)
+#var1 <- c(1,23,4,55,6,7,89)
 #let's print them and see
-var1
+#var1
 
 #difference in the largest and smallest value of the factors
-max(var1) - min(var1)
-mean(var1)
+#max(var1) - min(var1)
+#mean(var1)
 
 #time to find out if we can see any such examples in the in-built R data examples
 #View(mtcars)
 
 #say I consider horse power to be one of the factors in any x analysis
-(max_hp <- max(mtcars$hp))
-(min_hp <- min(mtcars$hp))
-(mean_hp <- mean(mtcars$hp))
+#(max_hp <- max(mtcars$hp))
+#(min_hp <- min(mtcars$hp))
+#(mean_hp <- mean(mtcars$hp))
 
-plot_them <- c(max_hp, mean_hp, min_hp)
-plot_them
+#plot_them <- c(max_hp, mean_hp, min_hp)
+#plot_them
 
-barplot(plot_them, main="horse-power", xlab="Max - Mean - Min")
+#barplot(plot_them, main="horse-power", xlab="Max - Mean - Min")
 
 #' A function to plot the maximum, mean and minimum values of a numerical factor
 #'
